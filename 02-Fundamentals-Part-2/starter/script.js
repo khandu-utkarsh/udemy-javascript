029972fe1254d785b88b7e3a53bd6dbafa9da819
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-01-23 22:36:07 -0500

    Watched and experimented with the javascript fundamentals

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const jonas = {
+  firstName: "Utkarsh",
+  lastName: "Khandelwal",
+  birthYear: 1996,
+  job: "sde",
+  friends: ["Chitvan", "Kundu", "Phogat"],
+  hasDriversLicense: true,
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+};
+
+console.log(jonas.calcAge());
+console.log(jonas["age"]);
